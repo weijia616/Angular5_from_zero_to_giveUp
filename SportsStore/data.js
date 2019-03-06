fdcb27696aef6e2525835db2d8c056c870aa1933
weijiazhang616 <weijiazhang616@gmail.com>
2019-03-05 21:56:50 -0500

    set data and authMiddleware.js

---
+++ SportsStore/data.js
@@ -0,0 +1,34 @@
+module.exports = function () {
+  return {
+    products: [
+      { id: 1, name: "Kayak", category: "Watersports",
+        description: "123", price: 275
+      },
+      { id: 2, name: "Lifejacket", category: "Watersports",
+        description: "234", price: 48.99
+      },
+      { id: 3, name: "Soccer Ball", category: "Soccer",
+        description: "345", price: 19.99
+      },
+      { id: 4, name: "Coner Flags", category: "Soccer",
+        description: "456", price: 0.01
+      },
+      { id: 5, name: "Stadium", category: "Soccer",
+        description: "567", price: 199
+      },
+      { id: 6, name: "Thinking CAP", category: "Chess",
+        description: "567", price: 29.99
+      },
+      { id: 7, name: "Unsteady Chair", category: "Chess",
+        description: "567", price: 0.01
+      },
+      { id: 8, name: "Chess board", category: "Chess",
+        description: "456", price: 1999
+      },
+      { id: 9, name: "King", category: "Chess",
+        description: "999", price: 10000
+      },
+    ],
+    orders: []
+  }
+};
