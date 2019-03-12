c6b4f9a0938ec336d94033f1dbb29c50220dcc93
weijiazhang616 <weijiazhang616@gmail.com>
2019-03-11 23:27:49 -0400

    add model.module.ts

---
+++ SportsStore/src/app/model/model.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from "@angular/core";
+import {ProductRepository} from "./product.repository";
+import {StaticDatasource} from "./static.datasource";
+
+@NgModule({
+  providers: [ProductRepository, StaticDatasource]
+})
+
+export class ModelModule{}
