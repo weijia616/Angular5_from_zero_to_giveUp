c91af97bde53c7ab91ec93c2d7328896f206b3e2
weijiazhang616 <weijiazhang616@gmail.com>
2019-03-13 23:16:03 -0400

    create store component

---
+++ SportsStore/src/app/store/store.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {ModelModule} from "../model/model.module";
+import {StoreComponent} from "./store.component";
+
+@NgModule({
+  // imports 告诉angular 该模块依赖以下
+  imports: [ModelModule, BrowserModule, FormsModule],
+  //declarations告诉angular关于StoreComponent类的属性
+  declarations: [StoreComponent],
+  //exports告诉angular StoreComponent也可以用于应用程序的其它部分，
+  exports: [StoreComponent]
+})
+
+export class StoreModule {}
