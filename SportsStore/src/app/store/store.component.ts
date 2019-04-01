2e7fa66940d7eebea5938394128ec39d6f195ed5
weijiazhang616 <weijiazhang616@gmail.com>
2019-03-31 22:43:07 -0400

    create DIY directive

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -42,4 +42,9 @@ export class StoreComponent{
     return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
       .fill(0).map((x, i) => i + 1);
   }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository.getProducts(this.selectedCategory).length /
+                      this.productsPerPage);
+  }
 }
--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -3,12 +3,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {ModelModule} from "../model/model.module";
 import {StoreComponent} from "./store.component";
+import {CounterDirective} from "./counter.directive";
 
 @NgModule({
   // imports 告诉angular 该模块依赖以下
   imports: [ModelModule, BrowserModule, FormsModule],
   //declarations告诉angular关于StoreComponent类的属性
-  declarations: [StoreComponent],
+  declarations: [StoreComponent, CounterDirective],
   //exports告诉angular StoreComponent也可以用于应用程序的其它部分，
   exports: [StoreComponent]
 })
