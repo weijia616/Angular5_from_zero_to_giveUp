5be321134280b4fa56a63fe94fce6dff3a6cec2f
weijiazhang616 <weijiazhang616@gmail.com>
2019-04-02 22:46:09 -0400

    create cartSummary component

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from "@angular/core";
 import {ProductRepository} from "./product.repository";
 import {StaticDatasource} from "./static.datasource";
+import {Cart} from "./cart.model";
 
 @NgModule({
-  providers: [ProductRepository, StaticDatasource]
+  providers: [ProductRepository, Cart]
 })
 
 export class ModelModule{}
--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -4,12 +4,13 @@ import {FormsModule} from "@angular/forms";
 import {ModelModule} from "../model/model.module";
 import {StoreComponent} from "./store.component";
 import {CounterDirective} from "./counter.directive";
+import {CartSummaryComponent} from "./cartSummary.component";
 
 @NgModule({
   // imports 告诉angular 该模块依赖以下
   imports: [ModelModule, BrowserModule, FormsModule],
-  //declarations告诉angular关于StoreComponent类的属性
-  declarations: [StoreComponent, CounterDirective],
+  //declarations告诉angular关于StoreComponent...类的属性
+  declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
   //exports告诉angular StoreComponent也可以用于应用程序的其它部分，
   exports: [StoreComponent]
 })
