903c9a0ebb3174a0bb7ad294b08f9e0026871d68
weijiazhang616 <weijiazhang616@gmail.com>
2019-04-05 23:04:05 -0400

    finish router guard and cart detail html

--- SportsStore/src/app/app.module.ts
+++ SportsStore/src/app/app.module.ts
@@ -9,22 +9,32 @@ import {RouterModule} from "@angular/router";
 import {StoreComponent} from "./store/store.component";
 import {CartDetailComponent} from "./store/cartDetail.component";
 import {CheckoutComponent} from "./store/checkout.component";
+import {StoreFirstGuard} from "./storeFirst.guard";
 
 
 @NgModule({
   imports: [
     BrowserModule, StoreModule,
     RouterModule.forRoot([
-      {path: "store", component: StoreComponent},
-      {path: "cart", component: CartDetailComponent},
-      {path: "checkout", component: CheckoutComponent},
+      {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
       {path: "**", redirectTo: "/store"}
     ])
   ],
+  providers: [StoreFirstGuard],
   declarations: [
     AppComponent
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- SportsStore/src/app/store/cartDetail.component.ts
+++ SportsStore/src/app/store/cartDetail.component.ts
@@ -1,10 +1,14 @@
 import {Component} from "@angular/core";
+import {Cart} from "../model/cart.model";
 
 
 @Component({
-  template: `<div><h3 class="bg-info p-1">Cart Detail Component</h3></div>`
+  template: `<div><h3 class="bg-info p-1">Cart Detail Component</h3></div>`,
+  templateUrl: "cartDetail.component.html"
 })
 
 export class CartDetailComponent {
+  constructor(public cart: Cart) {
 
+  }
 }
