5d63c4661ea9472bbcf2da21aebbf6ad90d4d5fd
weijiazhang616 <weijiazhang616@gmail.com>
2019-04-07 23:22:41 -0400

    update model module

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from "@angular/core";
 import {ProductRepository} from "./product.repository";
 import {StaticDatasource} from "./static.datasource";
 import {Cart} from "./cart.model";
+import {Order} from "./order.model";
+import {OrderRepository} from "./order.repository";
 
 @NgModule({
-  providers: [ProductRepository, StaticDatasource, Cart]
+  providers: [ProductRepository, StaticDatasource, Cart, Order, OrderRepository]
 })
 
 export class ModelModule{}
