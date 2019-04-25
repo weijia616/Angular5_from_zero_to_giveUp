13d6be00ca8a8ce8f2221b68f01b89238ff19c7c
weijiazhang616 <weijiazhang616@gmail.com>
2019-04-24 23:06:11 -0400

    order table  finish

--- SportsStore/src/app/admin/orderTable.component.ts
+++ SportsStore/src/app/admin/orderTable.component.ts
@@ -1,9 +1,31 @@
 import {Component} from "@angular/core";
+import {OrderRepository} from "../model/order.repository";
+import {Order} from "../model/order.model";
 
 @Component({
-  template: `<div class="bg-info p-1"><h3>Order Table Placeholder</h3></div>`
+  moduleId: module.id,
+  templateUrl: "orderTable.component.html"
 })
 
 export class OrderTableComponent {
+  includeShipped = false;
 
+  constructor(private  repository: OrderRepository) {
+
+  }
+
+  //
+  getOrders() : Order[] {
+    return this.repository.getOrders()
+      .filter(o => this.includeShipped || !o.shipped)
+  }
+
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+
+  delete(id: number) {
+    this.repository.deleteOrder(id);
+  }
 }
