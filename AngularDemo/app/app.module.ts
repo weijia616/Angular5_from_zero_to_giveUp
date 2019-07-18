44ea3020f6bf87c6eb796180e52a11d43356bc04
weijiazhang616 <weijiazhang616@gmail.com>
2019-07-17 23:18:20 -0400

    bring FormModule in the application

--- AngularDemo/app/app.module.ts
+++ AngularDemo/app/app.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {ProductComponent} from "./component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     declarations: [ProductComponent],
     bootstrap: [ProductComponent]
 })
