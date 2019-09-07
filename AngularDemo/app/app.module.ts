9f2b5725f55ee579d4fd18cca07dd0bac7c3b3e8
weijiazhang616 <weijiazhang616@gmail.com>
2019-09-07 14:47:34 -0400

    create first self directive: pa-attr

--- AngularDemo/app/app.module.ts
+++ AngularDemo/app/app.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {ProductComponent} from "./component";
 import {FormsModule} from "@angular/forms";
+import {PaAttrDirective} from "./attr.directive";
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [ProductComponent],
+    declarations: [ProductComponent, PaAttrDirective],
     bootstrap: [ProductComponent]
 })
 
